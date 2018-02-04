86376918d108eaab15a0f5a718ee2757d945282f
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-02-04 17:45:48 +0200

    methods return

--- Ruby Basics/methods.rb
+++
@@ -1,8 +0,0 @@
-def add(a, b)
-  puts "Adding #{a} and #{b}:"
-  puts a + b
-end
-
-add(2, 3)
-add(5, 5)
-add(6, 6)
---
+++ RubyBasics/methods.rb
@@ -0,0 +1,10 @@
+# Print out that we are adding two numbers together.
+# And then return the sun of the two numbers.
+def add(a, b)
+  puts "Adding #{a} and #{b}:"
+  return a + b
+end
+
+puts add(2, 3)
+add(5, 5)
+add(6, 6)
