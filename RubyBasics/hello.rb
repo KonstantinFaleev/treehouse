11db459a9f85a72934f300fa5a1fd43f159bea8c
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-02-19 23:44:23 +0200

    99 bottle

--- RubyBasics/hello.rb
+++ RubyBasics/hello.rb
@@ -13,3 +13,15 @@ puts "The year is #{year}"
 
 future = 5
 puts "In #{future} year, the year will be #{year + future}"
+
+number = 99
+while number > 1
+  puts number.to_s + ' bottles of beer on the wall'
+  sleep 0.5
+  puts number.to_s + ' bottles of beer!'
+  sleep 0.5
+  puts 'Take one down, pass it around'
+  number = number - 1
+  sleep 0.5
+  puts number.to_s + ' bottles of beer on the wall!'
+end
